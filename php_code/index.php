146c1afc89b761d6423510e8111f88b432e4c821
Nanfei Chen <chennanfei2006@163.com>
2014-12-27 19:52:25 +0800

    tried to get access token

--- php_code/index.php
+++ php_code/index.php
@@ -1,17 +1,21 @@
 <?php
 
-
 const CLIENT_ID = '44f394bbec284f60b224';
 const CLIENT_SECRET = '72f315fda524369a236a01c2d60bf9ab64a0474d';
 const SCOPE = 'repo';
 const AUTH_URI = 'https://github.com/login/oauth/authorize';
-const REDIRECT_URI = 'redirect_uri';
+const REDIRECT_URI = 'http://www.ucdream.com/oauth/github.php';
+const STATE = '123456789';
 
-$authUrl = AUTH_URI . '?' . 'client_id=' . CLIENT_ID
-    . '&client_secret=' .CLIENT_SECRET
-    . '&scope=' . SCOPE
-    . '&redirect_uri=' . REDIRECT_URI
-    . '&state=123456789';
+$data = http_build_query(array(
+    'client_id' => CLIENT_ID,
+    'client_secret' => CLIENT_SECRET,
+    'state' => STATE,
+    'redirect_uri' => REDIRECT_URI,
+    'scope' => SCOPE
+));
+
+$authUrl = AUTH_URI . "?$data";
 
 ?>
 
--- php_code/oauth/github.php
+++ php_code/oauth/github.php
@@ -1,5 +1,41 @@
 <?php
 
+const CLIENT_ID = '44f394bbec284f60b224';
+const CLIENT_SECRET = '72f315fda524369a236a01c2d60bf9ab64a0474d';
+const SCOPE = 'repo';
+const AUTH_URI = 'https://github.com/login/oauth/access_token';
+const REDIRECT_URI = 'http://www.ucdream.com/oauth/github.php';
+const STATE = '123456789';
 
+$paramList = array_merge($_POST, $_GET);
+print_r($paramList, true);
+print_r('<br>');
+
+if ($paramList['code'] != STATE) {
+    print_r('Code failed to pass verification');
+    return;
+}
+
+print_r('Code was verified. Retrieving access token...<br>');
+    
+// send post request and get access token
+$data = http_build_query(array(
+    'client_id' => CLIENT_ID,
+    'client_secret' => CLIENT_SECRET,
+    'code' => STATE,
+    'redirect_uri' => REDIRECT_URI
+));
+
+$context = stream_context_create(array(
+    'http' => array(
+        'content' => $data,
+        'header' => "Content-type: application/x-www-form-urlencoded\r\nContent-Length:" . strlen($data) . "\r\n",
+        'method' => 'POST',
+        'User-Agent' => 'ucdream'
+    )
+));
+
+$result = file_get_contents(AUTH_URI, false, $context);
+print_r($result);
 
 ?>
