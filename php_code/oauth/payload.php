95ba9c5a4d5133d4b936e6bc27558630088c9dc5
Nanfei Chen <chennanfei2006@163.com>
2014-12-28 18:41:13 +0800

    changed response payload format

--- php_code/oauth/payload.php
+++ php_code/oauth/payload.php
@@ -1,4 +1,4 @@
 <?php
-print_r('recied payload:<br>');
-print_r(json_decode(file_get_contents("php://input")));
+print_r('recieved payload:<br>');
+print_r(json_decode(file_get_contents("php://input"), true));
 ?>
