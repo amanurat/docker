6d15219d793ed082753158baaf733f93baed0453
Nanfei Chen <chennanfei2006@163.com>
2014-12-28 18:20:54 +0800

    fixed a bug

--- php_code/build.sh
+++ php_code/build.sh
@@ -30,6 +30,6 @@ sudo docker build -t $imageRepo .
 
 # start the image
 echo starting new contianer
-containerId=`sudo docker run -v -v /home/ubuntu/apache2:/var/log/apache2 -d -p 80:80 --name $container $imageRepo`
+containerId=`sudo docker run -v /home/ubuntu/apache2:/var/log/apache2 -d -p 80:80 --name $container $imageRepo`
 
 echo Successfully update image $imageRepo and restarted the container $container
